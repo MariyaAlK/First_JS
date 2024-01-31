09ef67c893c521df5610c894ce90eb3cbe211993
Мария Колесникова <madam.mashe@mail.ru>
2024-01-31 21:29:00 +0400

    Add task 6

--- JS.js
+++ JS.js
@@ -61,29 +61,79 @@
 // // console.log(rav);
 // console.log(a===b);
 
-console.log('Задание 5:');
-let a = 1;
-let out =  document.querySelector('.out');
+// console.log('Задание 5:');
+// let a = 1;
+// let out =  document.querySelector('.out');
 
-while (a < 51){
-    out.innerHTML += `${a++}; `
+// while (a < 51){
+//     out.innerHTML += `${a++}; `
+// }
+
+// out.innerHTML += `<br><br>`
+// for (let i=50; i>=25; i -=1){
+//     out.innerHTML += `${i}; `
+// }
+
+// out.innerHTML += `<br><br>`
+// // let b=2501;
+// // do{
+// //     out.innerHTML += `${b}; `
+// //     b +=2
+// // } while (b<=3000)
+
+// for (let i=2500; i<=3000; i++ ){
+//     if(i % 2 == 0){
+//         continue
+//     }
+//     out.innerHTML += `${i}; `
+// }
+
+console.log('Задание 6:');
+console.log('Задача 1:');
+let a = 5;
+let b = prompt('Введи значение переменной b');
+console.log('Значение а = ', a);
+console.log('Значение b = ', b);
+console.log('Они равны?');
+switch(b){
+    case `${a}`: 
+        console.log('Да')
+        break
+    case null:
+    case '':
+        console.log('Не сработало')
+        break
+    default:
+        console.log('Нет')
 }
 
-out.innerHTML += `<br><br>`
-for (let i=50; i>=25; i -=1){
-    out.innerHTML += `${i}; `
+console.log('Задача 2:');
+let c = prompt('Введи значение от 1 до 12');
+console.log('Введенное число ', c);
+switch(+c){
+    case 1:
+    case 2:
+    case 12: 
+        console.log('Зима')
+        break
+    case 3:
+    case 4:
+    case 5:        
+        console.log('Весна')
+        break
+    case 6:
+    case 7:
+    case 8:        
+        console.log('Лето')
+        break
+    default:      
+        console.log('Осень')
 }
 
-out.innerHTML += `<br><br>`
-// let b=2501;
-// do{
-//     out.innerHTML += `${b}; `
-//     b +=2
-// } while (b<=3000)
-
-for (let i=2500; i<=3000; i++ ){
-    if(i % 2 == 0){
-        continue
-    }
-    out.innerHTML += `${i}; `
-}
+console.log('Задача 3:');
+let var1 = null
+let var2 = 3
+let var3 = 'Hello'
+console.log(var1 + ', ' + var2 + ', ' + var3);
+console.log('Проверка с помощью оператора нулевого слияния:');
+console.log(var1 ?? var2 ?? var3);
